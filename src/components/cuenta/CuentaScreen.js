d0a16d6a14a8bb1ec1b886f740b8684b0af9bce8
TiagoLongueira <tlongueira@cdt.com.ar>
2021-11-12 21:42:03 -0300

    Cambios en diseño

--- src/components/cuenta/CuentaScreen.js
+++ src/components/cuenta/CuentaScreen.js
@@ -1,9 +1,58 @@
 import React from 'react'
+import profile from '../../assets/profile.jpg'
+import './cuentaScreen.css'
 
-export const CuentaScreen = () => {
+export const CuentaScreen = ({ history }) => {
+    const handleReturn = () => {
+
+        if (history.length <= 2) {
+            history.push('/');
+        } else {
+            history.goBack();
+        }
+
+    }
     return (
-        <div>
-            MI CUENTA
+        <div className="row mt-5">
+            <div className="col-3">
+            </div>
+
+            <div className="col-6 animate__animated animate__fadeIn">
+                <img
+                    src={profile}
+                    alt={"FOTO DE PERFIL"}
+                    className="img-thumbnail fotoPerfil mx-auto d-block animate__animated animate__fadeInLeft "
+                />
+                <h3 className="text-center mt-5"> NOMBRE USUARIO </h3>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item"> <b> Alter ego: </b> EDAD </li>
+                    <li className="list-group-item"> <b> Publisher: </b> DNI </li>
+                    <li className="list-group-item"> <b> First appearance: </b> EMAIL </li>
+                </ul>
+                <hr />
+                <h3 className="text-center mt-2"> DATOS DE ACCESO </h3>
+                <ul className="list-group list-group-flush mt-5">
+                    <li className="list-group-item"> <b> Alter ego: </b> NICK </li>
+                    <li className="list-group-item"> <b> Publisher: </b> CLAVE </li>
+                </ul>
+
+                <h5 className="text-center mt-2"> ACCIONES </h5>
+                <div class="btn-group d-flex justify-content-around" role="group" aria-label="Basic outlined example">
+                    <button type="button" class="btn btn-outline-primary acciones">Ver datos de Acceso</button>
+                    <button type="button" class="btn btn-outline-primary acciones">Editar mi Cuenta</button>
+                    <button type="button" class="btn btn-outline-danger acciones">Eliminar mi Cuenta</button>
+                </div>
+                <button
+                    className="btn btn-outline-info w-100 mt-4"
+                    onClick={handleReturn}
+                >
+                    Regresar a la página anterior!
+                </button>
+
+            </div>
+            <div className="col-3">
+            </div>
+
         </div>
     )
 }
--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import './Login.css';
 export const LoginScreen = ({ history }) => {
 
     const handleLogin = () => {
@@ -8,16 +9,59 @@ export const LoginScreen = ({ history }) => {
     }
 
     return (
-        <div className="container mt-5">
-            <h1>Login</h1>
-            <hr />
+        // <div className="container mt-15">
 
-            <button
-                className="btn btn-primary"
-                onClick={ handleLogin }
-            >
-                Login
-            </button>
+        //     <div >
+        //         <form>
+        //             <label>
+        //                 <p>Nickname</p>
+        //                 <input type="text" />
+        //             </label>
+        //             <label>
+        //                 <p>Clave</p>
+        //                 <input type="password" />
+        //             </label>
+        //             <div>
+
+        //             </div>
+        //         </form>
+        //     </div>
+        // </div>
+        <div className="row mt-5">
+            <div className="col-3">
+            </div>
+
+            <div className="col-6 animate__animated animate__fadeIn">
+
+                <div class="card text-center">
+                    <div class="card-header">
+                        Ingresá a la Aplicación!
+                    </div>
+                    <div class="card-body">
+                        <form>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Ingresá tu Nickname</label>
+                                <input type="text" class="form-control" id="inputNickname"  placeholder="nickname..."/>
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">Ingresá tu Clave</label>
+                                <input type="password" class="form-control" id="inputClave" placeholder="clave..."/>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="card-footer text-muted">
+                        <button
+                            className="btn btn-primary"
+                            onClick={handleLogin}
+                        >
+                            Ingresar
+                        </button>
+                    </div>
+                </div>
+
+            </div>
+            <div className="col-3">
+            </div>
 
         </div>
     )
